6d49d7caa83cf7b3d916df2f87af3968f9d1309b
Andres802 <reachandres@gmail.com>
2021-01-28 01:58:01 +0000

    cors hhtp cross response

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ from api.v1.views import app_views
 from os import getenv
 from flask import Flask, make_response, jsonify
 from flask import Blueprint
+from flask_cors import CORS
 
 """Host and port env variables"""
 host_env = getenv('HBNB_API_HOST') or '0.0.0.0'
@@ -13,6 +14,8 @@ port_env = getenv('HBNB_API_PORT') or 5000
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+""" Cors access to selected resources from a different origin."""
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
