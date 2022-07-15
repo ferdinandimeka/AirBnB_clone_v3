3b352ae852a055a066abc4107e3694c743748941
imekaferdy <imekaferdy20@gmail.com>
2022-07-15 01:41:46 +0000

    update

--- models/user.py
+++ models/user.py
@@ -8,6 +8,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import hashlib
 
+
 class User(BaseModel, Base):
     """Representation of a user """
     if models.storage_t == 'db':
@@ -16,9 +17,9 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user",
+        places = relationship("Place", backref="user",\
                 cascade="all, delete, delete-orphan")
-        reviews = relationship("Review", backref="user",
+        reviews = relationship("Review", backref="user",\
                 cascade="all, delete, delete-orphan")
     else:
         email = ""
